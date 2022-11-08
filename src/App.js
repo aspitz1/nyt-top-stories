63e5440e31a4b480d01f91162263fcf7ef65ca90
Anna Spitz <aspitzemail@gmail.com>
2022-11-08 14:23:09 -0600

    Add api call

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { useEffect, setState, useState } from "react";
+import getArticlesBySection from "./api-calls";
+
 function App() {
+  const [selectedSection, setSelectedSection] = useState("home");
+  const [currentArticles, setCurrentArticles] = useState(null);
+
+  useEffect(() => {
+    getArticlesBySection("home");
+  }, []);
+
   return (
     <>
-    <h1>Hello, World.</h1>
+      <h1>Hello, World.</h1>
     </>
   );
 }
